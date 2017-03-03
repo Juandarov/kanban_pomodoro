6b95470323802c4ec73c79c60d13af4c3142d718
juandarov <juandarov@gmail.com>
2017-03-03 09:51:33 +0100

    timer ok

--- static/js/script.js
+++ static/js/script.js
@@ -1,34 +1,44 @@
-$(document).ready(function(){
-    $(function(){
-        $("#hms_timer").countdowntimer({
-            minutes: 00,
-            seconds: 05,
-            size : "lg",
-            pauseButton : "pauseBtnhms",
-            stopButton : "stopBtnhms",
-            timeUp : timeisUp
-        });
-    });
+$(document).ready(function() {
+    // $(function() {
+    //     $("#hms_timer").countdowntimer({
+    //         minutes: 00,
+    //         seconds: 05,
+    //         size: "lg",
+    //         pauseButton: "pauseBtnhms",
+    //         stopButton: "stopBtnhms",
+    //         timeUp: timeisUp
+    //     });
+    // });
 
     $("#btnClicker").on("click", function() {
         $('ul').append('<li>' + '<div class="taches">' + '<p id="myDiv">' + '<span class="glyphicon glyphicon-remove-sign" aria-hidden="true"></span>' + $('#newtache').val() + '</p>' + '<div class="triangle_right"></div>' + '</div>' + '</li>');
 
         // TO CLEAR A TACHE
-        $(".glyphicon-remove-sign").click(function(){
+        $(".glyphicon-remove-sign").click(function() {
             $(this).parent().parent().parent().remove();
         });
 
         // MOVE TACHE TO EN COURS
-        $(".taches").click(function(){
-            $(this).parent().parent().parent().remove();
+        $(".taches").click(function() {
+            $(this).remove();
+            $(this).appendTo("#encours");
+            $(function() {
+                $("#hms_timer").countdowntimer({
+                    minutes: 25,
+                    seconds: 00,
+                    size: "lg",
+                    pauseButton: "pauseBtnhms",
+                    stopButton: "stopBtnhms"
+                    // $(this).remove();
+                });
+
+            })
         });
-
-
     });
 
     function timeisUp() {
-            alert("yolo")
-        }
+        // alert("yolo")
+    }
 
 
 });
